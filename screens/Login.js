8d8a1efe5afa6f317c1318e9fff83d492942f7fd
TrinhNgocBaoBaro <105273438+TrinhNgocBaoBaro@users.noreply.github.com>
2024-03-13 15:45:13 +0700

    Set up gooogle signin firebase auth

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,30 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 
 const Login = ({ navigation }) => {
   return (
     <View style={{flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center'}}>
-      <Button title='Đăng nhập' onPress={()=> navigation.navigate("Home")}/>
+      <TouchableOpacity
+          onPress={()=> navigation.navigate("Home")}
+          activeOpacity={0.7}
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: 20,
+            marginBottom: 40,
+            padding: 10,
+            width: 250,
+            backgroundColor: "#F5F5F5",
+            elevation: 3,
+            borderRadius: 10,
+          }}
+        >
+          <Image
+            source={require("../assets/GoogleLogin.png")}
+            style={{ height: 30, width: 30, borderRadius: 50, marginRight: 10 }}
+          />
+          <Text style={{ fontWeight: 'bold'}}>Đăng nhập với Google</Text>
+        </TouchableOpacity>
     </View>
   )
 }
